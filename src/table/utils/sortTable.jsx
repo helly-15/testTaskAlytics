3509961c9e74f26945cf8abb7a961b27ff3a5673
Lenocka <lvutina.en@gmail.com>
2020-12-25 00:54:47 +0300

    fix:add sorting all types

--- src/table/utils/sortTable.jsx
+++ src/table/utils/sortTable.jsx
@@ -1,14 +1,26 @@
 export default function sortTable(id,data,order) {
     let sorted;
+
     console.log(data);
     //let dataToSort =  data;
     if (order ==='asc'){
+
          sorted = data.sort((a,b)=>{
-            return a[id]-b[id]
+             if(typeof a[id] ==='string'){
+                 return a[id][0]- b[id][0]
+             } else {
+                 return a[id]-b[id]
+             }
+
         })
     } else {
-         sorted = data.sort((a,b)=>{
-            return b[id] - a[id]
+        sorted = data.sort((a,b)=>{
+            if(typeof a[id] ==='string'){
+                return b[id][0]- a[id][0]
+            } else {
+                return b[id]-a[id]
+            }
+
         })
     }
 
