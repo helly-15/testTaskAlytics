5cf58cbfdaccea9f0acb5350a63663f4576109b3
Lenocka <lvutina.en@gmail.com>
2020-12-24 11:45:49 +0300

    fix: format date to correct representation

--- src/table/utils/dataLine.jsx
+++ src/table/utils/dataLine.jsx
@@ -1,7 +1,7 @@
 import chain from '../../assets/chain.svg'
 function dataLine(pageNumber,numOfLines, data) {
     let pageTable =[];
-    console.log(pageNumber, numOfLines);
+    //console.log(pageNumber, numOfLines);
     for ( let i=(pageNumber -1)*numOfLines+1;i<=pageNumber*numOfLines;i++){
         pageTable.push(
             <tr>
@@ -9,7 +9,7 @@ function dataLine(pageNumber,numOfLines, data) {
                     <img src={chain} className='chain'/>
                     {data[i].number}
                 </th>
-                <td >{data[i].date}</td>
+                <td >{formatDate(data[i].date)}</td>
                 <td >{data[i].income}</td>
                 <td>{data[i].profit}</td>
                 <td>{data[i].daysTo}</td>
@@ -22,3 +22,25 @@ function dataLine(pageNumber,numOfLines, data) {
     ;
 }
 export default dataLine;
+
+function formatDate(dateToFormat) {
+    let parsed = new Date(Date.parse(dateToFormat));
+    let options = {
+        weekday: 'short',
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric'
+    };
+    let formatted = parsed.toLocaleDateString('ru-Ru', options);
+
+    let regexp = /[\s\u0433]\./g;
+    let regexp2 = /\s\,/g;
+    let regexp3 = /\./g;
+    let regexp4 = /\,/;
+    formatted = formatted.replace(regexp, '').replace(regexp2, ',').replace(regexp3, '').replace(regexp4, '.').replace(/\p{sc=Cyrillic}/u, l => l.toUpperCase()).replace(/\s\p{sc=Cyrillic}/u, l => l.toUpperCase());
+
+    return formatted;
+
+}
